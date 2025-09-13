ce7ab7e1ec662999f1d95bbfb8dc64ca139fbb90
Mike Rodriguez <michaelrodriguezphotography@gmail.com>
2025-09-12 18:05:43 -0700

    added flash to app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.utils import secure_filename
 from datetime import datetime
